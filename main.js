6b0ecc5f211465671060f420406585788dd16a2a
Jonathan Pena-Loa <jpena-loa@LABA.PSWAS.LOCAL>
2023-04-13 14:57:27 -0700

    added loop for for peropleArray without ships

--- main.js
+++ main.js
@@ -8,71 +8,73 @@ let getAndReturnPeople = async () => {
 
 // "Easy" - Loop through this array of objects and and log the name of each person
 
-// let arrayOfNamesShorterThan = [];
+let arrayOfNamesShorterThan = [];
 
 const getNamesOfPeopleFromApi = async () => {
     let peopleArray = await getAndReturnPeople();
 
-    // peopleArray.forEach(people => console.log(people.name));
+    peopleArray.forEach(people => console.log(people.name));
 
-    // peopleArray.forEach(names => {
-    //     if (names.height <= 165) {
-    //         arrayOfNamesShorterThan.push(names);
-    //         console.log("Medium", arrayOfNamesShorterThan);
-    //     }
-    // });
+    peopleArray.forEach(names => {
+        if (names.height <= 165) {
+            arrayOfNamesShorterThan.push(names);
+            console.log("Medium", arrayOfNamesShorterThan);
+        }
+    });
 
 
-    // let genderHiegths = {
-    //     talestFemales: undefined,
-    //     shortestFemales: undefined,
-    //     talestMale: undefined,
-    //     shortestMale: undefined,
-    // };
+    let genderHiegths = {
+        talestFemales: undefined,
+        shortestFemales: undefined,
+        talestMale: undefined,
+        shortestMale: undefined,
+    };
 
-    // peopleArray.forEach(person => {
-    //     if(person.gender === "male") {
-    //         if(genderHiegths.talestMale === undefined) {
-    //             genderHiegths.talestMale = +person.height
-    //             genderHiegths.shortestMale = +person.height
-    //         } else {
-    //             if(person.height > genderHiegths.talestMale ) {
-    //                 genderHiegths.talestMale = +person.height
-    //             }
-    //             if(person.height < genderHiegths.shortestMale) {
-    //                 genderHiegths.shortestMale = +person.height
-    //             }
-    //         }
+    peopleArray.forEach(person => {
+        if(person.gender === "male") {
+            if(genderHiegths.talestMale === undefined) {
+                genderHiegths.talestMale = +person.height
+                genderHiegths.shortestMale = +person.height
+            } else {
+                if(person.height > genderHiegths.talestMale ) {
+                    genderHiegths.talestMale = +person.height
+                }
+                if(person.height < genderHiegths.shortestMale) {
+                    genderHiegths.shortestMale = +person.height
+                }
+            }
     
-    //     } else if (person.gender === "female") {  
-    //         if(genderHiegths.talestFemales === undefined) {
-    //             genderHiegths.talestFemales = +person.height
-    //             genderHiegths.shortestFemales = +person.height
-    //         } else {
-    //             if(person.height > genderHiegths.talestFemales) {
-    //                 genderHiegths.talestFemales = +person.height;
-    //             } 
-    //             if(person.height < genderHiegths.shortestFemales) {
-    //                 genderHiegths.shortestFemales = +person.height;
-    //             }
-    //         }      
-    //     } 
+        } else if (person.gender === "female") {  
+            if(genderHiegths.talestFemales === undefined) {
+                genderHiegths.talestFemales = +person.height
+                genderHiegths.shortestFemales = +person.height
+            } else {
+                if(person.height > genderHiegths.talestFemales) {
+                    genderHiegths.talestFemales = +person.height;
+                } 
+                if(person.height < genderHiegths.shortestFemales) {
+                    genderHiegths.shortestFemales = +person.height;
+                }
+            }      
+        } 
 
-    // });
-    // console.log(genderHiegths);
+    });
+    console.table(genderHiegths);
     // console.log(peopleArray);
 
     peopleArray.forEach(person => {
 
-        if(person.starships.length === 0) {
-            console.log(`This persin is ${person.name} and they have no ship.`);
-        }
-
+      if(person.starships.length === 0) {
+          console.log(`This persin is ${person.name} and they have no ship.`);
+      } else {
         person.starships.forEach(ship => {
           fetch(ship).then(res => res.json()).then( data => {
-            console.log(`This person ${person.name} and their ships is ${data.name}`);
-          })
-        });
+              console.log(`This person ${person.name} and their ships is ${data.name}`);
+  
+            })
+          });
+      }
+       
     })
     
 }
